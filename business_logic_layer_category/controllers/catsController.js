0f51c317a9588ae794264f385b8347c6ec74a0b4
Lei Xu <lxu@pivotal.io>
2015-06-23 18:02:23 +0900

    step1 WIP, category modified

--- business_logic_layer_category/controllers/catsController.js
+++ business_logic_layer_category/controllers/catsController.js
@@ -1,54 +1,10 @@
-var Q = require("q");
-var mongojs = require('mongojs');
-var mongodbManager = require('../utils/mongodbManager');
-
-
+var dummy_data = require('../dummy_data/categories.js');
 
 /*************************************/
 /* REST API controller getCategories */
 exports.getCategories = function (req, res) {
-  console.log("Begin: getCategories");
-  console.log("Before getting catList: " + (new Date()).toISOString());
-
-  var db = mongodbManager.getConnection(["category"]);
-
-  getCategoriesPromise(db)
-    .then(function(catList) {
-      console.log("After getting catList: " + (new Date()).toISOString());
-      console.log("catList:");
-      console.log(catList);
-
-      res.set('Content-Type', 'application/json');
-      res.send(catList);
-    })
-    .done(function() {
-      console.log("getCategories mongodb close");
-      db.close();
-      console.log("End: getCategories");
-    });
-};
-
-exports.getCategoriesPromise = function(db) {
-  return getCategoriesPromise(db);
-};
-
-var getCategoriesPromise = function(db) {
-  var d = Q.defer();
-
-  db.category.findOne(function(err, catList) {
-    if(err || !catList) 
-    {
-      d.reject(new Error(err));
-    }
-    else 
-    {
-      console.log("catList found");
-      d.resolve(catList);
-
-    }
-  });
-
-  return d.promise;
+  res.set('Content-Type', 'application/json');
+  res.send(dummy_data);
 };
 /* REST API controller getCategories */
 /*************************************/
